8e812a5cc82e114cff7653ca85a00b7071d794bf
Mousa16 <mahmoudmousa124@gmail.com>
2023-08-28 15:11:09 +0300

    First lesson in embedded c

---
+++ Embedded_C_Course/Unit3_Lesson1/Static library/can.c
@@ -0,0 +1,6 @@
+#include<can.h>
+#include<stdio.h>
+void can_init()
+{
+	printf("You are in can_init.\n");
+}
---
+++ Embedded_C_Course/Unit3_Lesson1/Static library/main.c
@@ -0,0 +1,7 @@
+#include <stdio.h>
+
+int main(int argc, char const *argv[])
+{
+	can_init();
+	return 0;
+}
